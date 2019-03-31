0000768abda9903becb0c97dde4c2ead97b0a449
PaniniSama <saad.bah.01@gmail.com>
2019-03-31 14:17:30 +0000

    firstpaninicontribution

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaDepecheDunord
                 </div>
 
                 <div class="links">
